80c4eaecb866e5cd1408f52ffca9c1f904007208
Mbor17 <borovetc@gmail.com>
2023-05-02 00:19:51 +0300

    lesson2-hw

---
+++ src/components/Posts.js
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from 'react';
+import axios, {post} from "axios";
+import Post from "./Post";
+
+const Posts = () => {
+    const [posts, setPosts] = useState([]);
+    const [post, setPost] = useState(null);
+    useEffect(()=>{
+        axios('https://jsonplaceholder.typicode.com/posts').then(value => value.data).then(value => setPosts(value))
+    },[])
+    return (
+        <div>
+            {post&&
+                <div>
+                    <div>id:{post.id}</div>
+                    <div>title:{post.title}</div>
+                    <div>body:{post.body}</div>
+                    <div>userId:{post.userId}</div>
+            </div>}
+            {posts.map(post=><Post key={post.id} post={post} setPost={setPost}/>)}
+        </div>
+    );
+};
+
+export default Posts;
